fb6cd297da0c420317dc58abb5824e74bc7670ac
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2011-07-06 14:15:36 +0200

    Add more tests.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polyconseil.fr>

--- tests/test_base.py
+++ tests/test_base.py
@@ -20,9 +20,35 @@ class WorkflowDeclarationTestCase(unittest2.TestCase):
         self.assertEqual(3, len(MyWorkflow.states))
         self.assertEqual(3, len(MyWorkflow.transitions))
         self.assertEqual(MyWorkflow.states['foo'], MyWorkflow.initial_state)
+        self.assertEqual([MyWorkflow.states['foo']], MyWorkflow.transitions['foobar'].source)
+        self.assertEqual(MyWorkflow.states['bar'], MyWorkflow.transitions['foobar'].target)
 
         for state in MyWorkflow.states:
             self.assertEqual(state.name, state.title)
+            self.assertIn(state.name, ('foo', 'bar', 'baz'))
+
+    def test_object_definition(self):
+        class MyWorkflow(base.Workflow):
+            states = (
+                base.State('foo', 'Foo'),
+                base.State('bar', 'Bar'),
+                base.State('baz', 'Baz'),
+            )
+            transitions = (
+                base.TransitionDef('foobar', 'foo', 'bar'),
+                base.TransitionDef('gobaz', ('foo', 'bar'), 'baz'),
+                base.TransitionDef('bazbar', 'baz', 'bar'),
+            )
+            initial_state = 'foo'
+
+        self.assertEqual(3, len(MyWorkflow.states))
+        self.assertEqual(3, len(MyWorkflow.transitions))
+        self.assertEqual(MyWorkflow.states['foo'], MyWorkflow.initial_state)
+        self.assertEqual([MyWorkflow.states['foo']], MyWorkflow.transitions['foobar'].source)
+        self.assertEqual(MyWorkflow.states['bar'], MyWorkflow.transitions['foobar'].target)
+
+        for state in MyWorkflow.states:
+            self.assertEqual(state.name.capitalize(), state.title)
             self.assertIn(state.name, ('foo', 'bar', 'baz'))
 
 
