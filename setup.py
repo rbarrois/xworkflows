c60ae7000603e624cacacef063cddff04c4fd8dc
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-03-26 21:00:35 +0200

    Refactory setup.py.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- setup.py
+++ setup.py
@@ -1,29 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from distutils.core import setup
+from distutils import cmd
 import os
-from setuptools import setup
+import re
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def get_version():
+    version_re = re.compile(r"^VERSION = '([\w_.]+)'$")
+    with open(os.path.join(os.path.dirname(__file__), 'xworkflows', '__init__.py')) as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    return '0.0'
+
+
+class test(cmd.Command):
+    """Run the tests for this package."""
+    command_name = 'test'
+    description = 'run the tests associated with the package'
+
+    user_options = [
+        ('test-suite=', None, "A test suite to run (defaults to 'tests')"),
+    ]
+
+    def initialize_options(self):
+        self.test_runner = None
+        self.test_suite = None
+
+    def finalize_options(self):
+        self.ensure_string('test_suite', 'tests')
+
+    def run(self):
+        """Run the test suite."""
+        import unittest
+        if self.verbose:
+            verbosity=1
+        else:
+            verbosity=0
+
+        suite = unittest.TestLoader().loadTestsFromName(self.test_suite)
+
+        unittest.TextTestRunner(verbosity=verbosity).run(suite)
+
 
 setup(
-    name = "xworkflows",
-    version = "0.1",
-    author = "Raphaël Barrois",
-    author_email = "raphael.barrois@polyconseil.fr",
-    description = ("A library implementing workflows (or state machines) "
+    name="xworkflows",
+    version=get_version(),
+    author="Raphaël Barrois",
+    author_email="raphael.barrois@polyconseil.fr",
+    description=("A library implementing workflows (or state machines) "
         "for Python projects."),
-    license = "BSD",
-    keywords = "workflow state machine automaton",
-    url = "http://packages.python.org/xworkflows",
-    package_dir = {'': 'src'},
-    packages = ['xworkflows'],
-    long_description=read('README.rst'),
+    license="BSD",
+    keywords=['workflow', 'state machine', 'automaton'],
+    url="http://github.com/rbarrois/xworkflows",
+    download_url="http://pypi.python.org/pypi/xworkflows/",
+    package_dir={'': 'src'},
+    packages=['xworkflows'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -32,6 +66,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    test_suite='tests',
-    test_requires=['unittest2'],
+    cmdclass={'test': test},
 )
--- src/xworkflows/__init__.py
+++ src/xworkflows/__init__.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2012 Raphaël Barrois
+
+__version__ = '0.2.1'
+__author__ = 'Raphaël Barrois <raphael.barrois@polytechnique.org>'
+
 from . import base
 
 AbortTransition = base.AbortTransition
