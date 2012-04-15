89ab53c1c9592393ae0c35411096c6fc94073ce3
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2012-04-15 22:30:45 +0200

    Version bump
    Signed-off-by: Raphaël Barrois <raphael.barrois@polyconseil.fr>

--- src/xworkflows/__init__.py
+++ src/xworkflows/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011-2012 Raphaël Barrois
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'Raphaël Barrois <raphael.barrois@polytechnique.org>'
 
 from . import base
