8fb70c8116ac7d24d16a9bf7bf194ac8d1fae1a6
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-04-30 01:37:44 +0200

    Version 0.3.0 ready for shipping.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- src/xworkflows/__init__.py
+++ src/xworkflows/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011-2012 Raphaël Barrois
 
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 __author__ = 'Raphaël Barrois <raphael.barrois@polytechnique.org>'
 
 from . import base
