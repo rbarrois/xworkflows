b783f15cfa97666729823f27c4d7c8f1d2c731ce
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2012-07-03 11:23:58 +0200

    Expose hook decorators.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polyconseil.fr>

--- src/xworkflows/__init__.py
+++ src/xworkflows/__init__.py
@@ -6,12 +6,22 @@ __author__ = 'Raphaël Barrois <raphael.barrois@polytechnique.org>'
 
 from . import base
 
+# Errors
 AbortTransition = base.AbortTransition
 ForbiddenTransition = base.ForbiddenTransition
 InvalidTransitionError = base.InvalidTransitionError
 WorkflowError = base.WorkflowError
 
+# Defining and applying workflows
 Workflow = base.Workflow
 WorkflowEnabled = base.WorkflowEnabled
 
+# Decorators
 transition = base.transition
+
+# Hooks
+before_transition = base.before_transition
+after_transition = base.after_transition
+transition_check = base.transition_check
+on_enter_state = base.on_enter_state
+on_leave_state = base.on_leave_state
