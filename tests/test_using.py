40161e886d52f3fa70724c696d3a44b9555b4eac
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-08-02 02:08:00 +0200

    Add test for deep inheritance.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- tests/test_using.py
+++ tests/test_using.py
@@ -401,6 +401,29 @@ class WorkflowEnabledTestCase(unittest.TestCase):
         obj.gobaz()
         self.assertEqual(self.MyWorkflow.states.baz, obj.state)
 
+    def test_deeply_inherited_implementation(self):
+        class MyObject(base.WorkflowEnabled):
+            state = self.MyWorkflow()
+
+            @base.transition()
+            def foobar(self):
+                return 42
+
+        class MySubObject(MyObject):
+            pass
+
+        class MySubSubObject(MySubObject):
+            pass
+
+        obj = MySubSubObject()
+        self.assertTrue(hasattr(MySubSubObject, '_workflows'))
+        self.assertIn('state', MySubSubObject._workflows)
+        self.assertEqual(self.MyWorkflow.states.foo, obj.state)
+        self.assertEqual(42, obj.foobar())
+        self.assertEqual(self.MyWorkflow.states.bar, obj.state)
+        obj.gobaz()
+        self.assertEqual(self.MyWorkflow.states.baz, obj.state)
+
     def test_inherited_overridden_implementation(self):
         class MyObject(base.WorkflowEnabled):
             state = self.MyWorkflow()
