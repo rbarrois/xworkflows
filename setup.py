6efd1a518838dba553b213a457e3b0b493378713
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2013-09-24 14:04:29 +0200

    Switch back to setuptools.

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@
 # Copyright (c) 2011-2013 Raphaël Barrois
 # This code is distributed under the two-clause BSD License.
 
-from distutils.core import setup
-from distutils import cmd
+from __future__ import unicode_literals
+
 import os
 import re
 import sys
 
+from setuptools import setup
+
 root_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_version():
-    version_re = re.compile(r"^__version__ = '([\w_.-]+)'$")
-    with open(os.path.join(root_dir, 'xworkflows', '__init__.py')) as f:
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    path_components = package_components + ['__init__.py']
+    with open(os.path.join(root_dir, *path_components)) as f:
         for line in f:
             match = version_re.match(line[:-1])
             if match:
@@ -22,58 +26,31 @@ def get_version():
     return '0.1.0'
 
 
-class test(cmd.Command):
-    """Run the tests for this package."""
-    command_name = 'test'
-    description = 'run the tests associated with the package'
-
-    user_options = [
-        ('test-suite=', None, "A test suite to run (defaults to 'tests')"),
-    ]
-
-    def initialize_options(self):
-        self.test_runner = None
-        self.test_suite = None
-
-    def finalize_options(self):
-        self.ensure_string('test_suite', 'tests')
-
-    def run(self):
-        """Run the test suite."""
-        import unittest
-        if self.verbose:
-            verbosity=1
-        else:
-            verbosity=0
-
-        ex_path = sys.path
-        sys.path.insert(0, root_dir)
-        suite = unittest.TestLoader().loadTestsFromName(self.test_suite)
-
-        unittest.TextTestRunner(verbosity=verbosity).run(suite)
-        sys.path = ex_path
-
+PACKAGE = 'xworkflows'
 
 setup(
     name="xworkflows",
-    version=get_version(),
+    version=get_version(PACKAGE),
     author="Raphaël Barrois",
     author_email="raphael.barrois+xworkflows@polytechnique.org",
     description=("A library implementing workflows (or state machines) "
         "for Python projects."),
     license="BSD",
     keywords=['workflow', 'state machine', 'automaton'],
-    url="http://github.com/rbarrois/xworkflows",
-    download_url="http://pypi.python.org/pypi/xworkflows/",
+    url="https://github.com/rbarrois/xworkflows",
+    download_url="https://pypi.python.org/pypi/xworkflows/",
     packages=['xworkflows'],
+    setup_requires=[
+        'setuptools>=0.8',
+    ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ],
-    cmdclass={'test': test},
+    test_suite='tests',
 )
