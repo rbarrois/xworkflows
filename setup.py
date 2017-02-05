c9905392ec9856e269e207d3a571f83be3a54068
Raphaël Barrois <raphael.barrois@polytechnique.org>
2017-02-05 15:34:26 +0100

    Modernize packaging
    * Source code in ``src/``: makes sure packaging is correct
    * Use tox for multi-version testing
    * Add a Makefile for simpler automation
    * Use flake8 for linting

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2011-2013 Raphaël Barrois
+# Copyright (c) The xworkflows project
 # This code is distributed under the two-clause BSD License.
 
 import codecs
@@ -8,40 +8,56 @@ import os
 import re
 import sys
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_version(package_name):
+def get_version():
     version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
-    package_components = package_name.split('.')
-    init_path = os.path.join(root_dir, *(package_components + ['__init__.py']))
-    with codecs.open(init_path, 'r', 'utf-8') as f:
-        for line in f:
-            match = version_re.match(line[:-1])
-            if match:
-                return match.groups()[0]
+    roots = [root_dir, os.path.join(root_dir, 'src')]
+    for root in roots:
+        version_path = os.path.join(root_dir, PACKAGE, 'version.py')
+        if not os.path.exists(version_path):
+            continue
+        with codecs.open(version_path, 'r', 'utf-8') as f:
+            for line in f:
+                match = version_re.match(line[:-1])
+                if match:
+                    return match.groups()[0]
     return '0.1.0'
+
+
+def clean_readme(fname):
+    """Cleanup README.rst for proper PyPI formatting."""
+    with codecs.open(fname, 'r', 'utf-8') as f:
+        return ''.join(
+            re.sub(r':\w+:`([^`]+?)( <[^<>]+>)?`', r'``\1``', line)
+            for line in f
+            if not (line.startswith('.. currentmodule') or line.startswith('.. toctree'))
+        )
 
 
 PACKAGE = 'xworkflows'
 
+
 setup(
-    name="xworkflows",
-    version=get_version(PACKAGE),
+    name=PACKAGE,
+    version=get_version(),
     author="Raphaël Barrois",
-    author_email="raphael.barrois+xworkflows@polytechnique.org",
-    description=("A library implementing workflows (or state machines) "
-        "for Python projects."),
-    license="BSD",
+    author_email='raphael.barrois_%s@polytechnique.org' % PACKAGE,
+    description="A library implementing workflows (or state machines) for Python projects.",
+    long_description=clean_readme('README.rst'),
+    license='BSD',
     keywords=['workflow', 'state machine', 'automaton'],
-    url="https://github.com/rbarrois/xworkflows",
-    download_url="https://pypi.python.org/pypi/xworkflows/",
-    packages=['xworkflows'],
-    setup_requires=[
-        'setuptools>=0.8',
+    url='https://github.com/rbarrois/%s' % PACKAGE,
+    install_requires=[
     ],
+    setup_requires=[
+        'setuptools>=1',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -49,7 +65,11 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     test_suite='tests',
 )
